1befbfbfb490f418725343eb77301f9f1f5ade7d
Joe Ho <jkho@basistech.com>
2016-09-19 11:02:10 -0400

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -4,10 +4,30 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello():
     provider = str(os.environ.get('PROVIDER', 'world'))
     return 'Hello '+provider+'!'
+
+def primes(n):
+    primfac = []
+    d = 2
+    while d*d <= n:
+        while (n % d) == 0:
+            primfac.append(d)  # supposing you want multiple factors repeated
+            n //= d
+        d += 1
+    if n > 1:
+       primfac.append(n)
+    return primfac
+
+@app.route('/busy')
+def hello():
+    while True:
+	r = random.random()
+	n = int(r * 100000000)
+    print n, primes(n)
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
