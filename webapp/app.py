e595ccd799b5193a17765f5dbd9d1d42da2ed495
Joe Ho <jkho@basistech.com>
2016-09-21 14:11:50 -0400

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -45,4 +45,5 @@ def stop():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
+    busy = False
     app.run(host='0.0.0.0', port=port, threaded=True)
